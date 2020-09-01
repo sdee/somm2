61f5f921113ac03c5c966a953e9f3fd378b8d3a4
sdee <thaiyoshi@gmail.com>
2020-09-01 01:05:28 -0700

    Hook up state with radar chart

--- src/App.js
+++ src/App.js
@@ -7,38 +7,12 @@ import { AttributeSlider } from "./components/AttributeSlider";
 import { Row, Col, Slider } from "antd";
 
 const dimensions =  ['berry', 'citrus', 'stonefruit', 'grassy', 'floral', 'spicy', 'mineral', 'sweet', 'sour', 'woody', 'tannic', 'body', 'balance', 'finish'];
-const data = {
-  labels: dimensions,
-  datasets: [{
-      label: 'Previous Tasting',
-      backgroundColor: 'rgba(255,99,132,0.2)',
-      borderColor: 'rgba(255,99,132,1)',
-      pointBackgroundColor: 'rgba(255,99,132,1)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgba(255,99,132,1)',
-      data: [5, 6, 10, 7, 8, 1, 9, 7, 5, 3, 6, 7, 9, 9]
-  }]
-}
-
-const options = {
-  scale: {
-      angleLines: {
-          display: false
-      },
-      ticks: {
-          suggestedMin: 0,
-          suggestedMax: 10
-      },
-      backgroundColor: 'rgba(0, 0, 0, 0.1)'
-  }
-};
 
 function App() {
   const initState ={ };
   dimensions.forEach(dimension => initState[dimension] = 0 );
   console.log(initState);
-  const [flavors, setFlavors] = useState({ left: 0, top: 0, width: 100, height: 100 });
+  const [flavors, setFlavors] = useState(initState);
   const setAttribute = (attribute, value) => {
     setFlavors(state => ({ ...state, [attribute]: value}));
   };
